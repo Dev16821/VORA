7bebdefe9edbb24b0e7f9cf061b021711309625f
Dev16821 <vicky168218@gmail.com>
2026-05-24 08:16:06 +0530

    VORA v0.1 backend prototype

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from core.command_router import handle_command
+from system.memory_cleaner import clean_python_cache
+from model.llama import ask_ollama
 
 
 def main():
@@ -7,12 +9,15 @@ def main():
     while True:
         command = input("VORA > ")
 
+        print(f"VORA > {ask_ollama(command)}")  # Print Ollama's response
+
         if command.lower().strip() == "exit":
             print("VORA shutting down.")
             break
 
-        handle_command(command)
+        #handle_command(command)
 
 
 if __name__ == "__main__":
+    clean_python_cache()
     main()
